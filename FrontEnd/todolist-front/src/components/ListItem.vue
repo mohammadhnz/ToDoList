b53af69493a7206874b528acb45dfee133065a37
Roya Ghavami <royaghavami@Royas-MacBook-Air.local>
2023-07-13 15:05:24 +0430

    feature: add list item component

---
+++ FrontEnd/todolist-front/src/components/ListItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in items" :key="item._id">
+        {{ item.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  mounted() {
+    axios.get('/api/todo')
+        .then(response => {
+          this.items = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+  }
+}
+</script>
