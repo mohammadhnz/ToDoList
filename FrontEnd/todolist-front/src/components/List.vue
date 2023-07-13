8ac1e391714ef819a509cd058507b5faa7d56721
Roya Ghavami <royaghavami@Royas-MacBook-Air.local>
2023-07-13 15:38:46 +0430

    feature: create todo list component

---
+++ FrontEnd/todolist-front/src/components/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>My Todo List</h1>
+    <todo-list @item-deleted="fetchItems"></todo-list>
+    <add-item @item-added="fetchItems"></add-item>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue';
+import AddItem from './components/AddItem.vue';
+
+export default {
+  components: {
+    TodoList,
+    AddItem
+  },
+  methods: {
+    fetchItems() {
+      // Fetch the list of items from the backend API
+    }
+  },
+  mounted() {
+    this.fetchItems();
+  }
+}
+</script>
